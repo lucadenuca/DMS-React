aa185edaf6f6f13dd6fa51761ac1ee328cff342d
lucadenuca <luka.filipovic.fon@gmail.com>
2022-09-03 14:30:56 +0200

    Sortiranje dokumenata za korisnike

--- src/pages/ViewDocuments.jsx
+++ src/pages/ViewDocuments.jsx
@@ -9,7 +9,8 @@ const ViewDocuments = () => {
     var korisnikId = localStorage.getItem('Id');
     const navigate = useNavigate();
     const [input, setInput] = useState('');
-
+    const [sortKolona, setSortKolona] = useState('');
+    const [sortPoredak, setSortPoredak] = useState('');
 
     useEffect(() => {
         axios.get(`api/get-documents/${korisnikId}`).then(res => {
@@ -41,6 +42,22 @@ const ViewDocuments = () => {
     }
 
 
+    function handleSortKolona(e) {
+        setSortKolona(e.target.value)
+    }
+
+    function handlePoredakSort(e) {
+        setSortPoredak(e.target.value)
+
+    }
+
+    function sort() {
+        axios.get(`api/sort-documents/${korisnikId}/${sortKolona}/${sortPoredak}`).then(res => {
+            setDocuments(res.data.documents);
+        });
+    }
+
+
 
     return (
         <div className="vd-div">
@@ -51,9 +68,27 @@ const ViewDocuments = () => {
 
             <Link to="/docs"><button className='btn btn-dark' id='add-btn'>ADD</button></Link>
 
-            <div className='sr-div'>
-                <input type="text" onChange={handleInput} value={input} className='form-control' id='in-txt-src' />
-                <button className='btn btn-dark' onClick={pretraga} id='btn-src'>Pretraga</button>
+            <div className='fl-div'>
+                <div className='sr-div'>
+                    <input type="text" onChange={handleInput} value={input} className='form-control' id='in-txt-src' />
+                    <button className='btn btn-dark' onClick={pretraga} id='btn-src'>Pretraga</button>
+                </div>
+                <div className='sr-div'>
+                    <select className='form-select' onChange={handleSortKolona} id='kol-s'>
+                        <option>Choose item</option>
+                        <option value="id">Document ID</option>
+                        <option value="naziv">Naziv</option>
+                        <option value="kategorija">Kategorija</option>
+                        <option value="opis">Opis</option>
+                        <option value="putanja">Putanja</option>
+                    </select>
+                    <select className='form-select' onChange={handlePoredakSort} id='kol-p'>
+                        <option>Choose item</option>
+                        <option value="asc">Rastuce</option>
+                        <option value="desc">Opadajuce</option>
+                    </select>
+                    <button className='btn btn-dark' onClick={sort} id='btn-src'>Sort</button>
+                </div>
             </div>
 
             <div className='tbl-div'>
